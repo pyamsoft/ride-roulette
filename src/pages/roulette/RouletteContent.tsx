c0c5df30c1a44b670322bd62f70adeedcd88b36b
pyamsoft <developer.pyamsoft@gmail.com>
2023-03-25 20:07:30 -0700

    Stop rain after 10 seconds

--- src/pages/roulette/RouletteContent.tsx
+++ src/pages/roulette/RouletteContent.tsx
@@ -175,6 +175,20 @@ export const RouletteContent: React.FunctionComponent<RouletteState> =
       }
     }, [selectedIndex, setRain]);
 
+    React.useEffect(() => {
+      if (!rain) {
+        return;
+      }
+
+      const timer = window.setTimeout(() => {
+        setRain(false);
+      }, 10000);
+
+      return () => {
+        window.clearTimeout(timer);
+      };
+    }, [rain, setRain]);
+
     return (
       <Box width="100%" height="100%" overflow="hidden" position="relative">
         <ReactConfetti
