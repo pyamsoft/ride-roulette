bae7dc32c27cb997506e9dcdf05d0676f9cee18e
pyamsoft <developer.pyamsoft@gmail.com>
2025-06-03 20:54:31 -0700

    Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@
 
 import { defineConfig } from "vitest/config";
 import { UserConfigExport } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
